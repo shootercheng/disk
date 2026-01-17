d5c0bcf06d2cebc2ca93c17ee54071a6546dc47a
shootercheng <3281328128@qq.com>
2026-01-17 09:47:07 +0800

    设置阈值

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 )
 
+var threshold_byte int64 = 5
+
 func ScanFileByPath(path string) int64 {
 	// fmt.Println("开始扫描文件夹:", path)
 	res, err := os.ReadDir(path)
@@ -17,24 +19,29 @@ func ScanFileByPath(path string) int64 {
 		if file.IsDir() {
 			currenDirPath := path + "\\" + file.Name()
 			dirSize := ScanFileByPath(currenDirPath)
-			fmt.Printf("文件夹:%s 包含文件大小为:%d \n", currenDirPath, dirSize)
+			if dirSize >= threshold_byte {
+				fmt.Printf("文件夹:%s 包含文件大小为:%d \n", currenDirPath, dirSize)
+			}
 			fileSumSize += dirSize
 			continue
 		}
 		filePath := path + "\\" + file.Name()
-		fmt.Println(filePath)
 		fileInfo, err := file.Info()
 		if err != nil {
 			fmt.Printf("获取文件信息:%s失败:%s\n", filePath, err.Error())
 		} else {
-			fileSumSize += fileInfo.Size()
+			fileSize := fileInfo.Size()
+			if fileSize > threshold_byte {
+				fmt.Printf("文件:%s 大小为:%d \n", filePath, fileSize)
+			}
+			fileSumSize += fileSize
 		}
 	}
 	return fileSumSize
 }
 
 func main() {
-	rootPath := "C:\\Users\\scd"
+	rootPath := "E:\\code\\go\\toolset\\root"
 	size := ScanFileByPath(rootPath)
 	fmt.Printf("文件夹%s包含文件大小为:%d \n", rootPath, size)
 }
