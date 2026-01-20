90c5f21cad86c14b5bf56138e831d3973d7b04b9
shootercheng <3281328128@qq.com>
2026-01-20 22:48:52 +0800

    fix:test

--- tests/locales/disk_locales_test.go
+++ tests/locales/disk_locales_test.go
@@ -3,12 +3,13 @@ package locales
 import (
 	"testing"
 
+	"github.com/shootercheng/disk/pkg/constants"
 	"github.com/shootercheng/disk/pkg/locales"
 )
 
 func TestLoadLocalesZh(t *testing.T) {
 	locales.LoadLocales("zh")
-	key := "system_info_msg"
+	key := constants.SYSTEM_INFO_MSG_KEY
 	val := locales.GetMsg(key)
 	if val != "当前系统类型:%s,文件路径分隔符:%s\n" {
 		t.Errorf("加载国际化key %s 失败", key)
@@ -17,9 +18,9 @@ func TestLoadLocalesZh(t *testing.T) {
 
 func TestLoadLocaleEn(t *testing.T) {
 	locales.LoadLocales("en")
-	key := "system_info_msg"
+	key := constants.SYSTEM_INFO_MSG_KEY
 	val := locales.GetMsg(key)
-	if val != "Current system type: %s,file path separator: %s\n" {
+	if val != "Current system type:%s,file path separator:%s\n" {
 		t.Errorf("加载国际化key %s 失败", key)
 	}
 }
